4d87c8e6d87da8fbd2682f44abf6f1a97b7aaed9
KafCoppelia <69038090+KafCoppelia@users.noreply.github.com>
2023-03-06 20:46:23 +0800

    0.4.0.post1 (#27)
    * 🐛 v0.4.0.post1: fix bug of not using local images
    
    * 🐛 v0.4.0.post1: touhou only has MajorArcana

--- nonebot_plugin_tarot/__init__.py
+++ nonebot_plugin_tarot/__init__.py
@@ -6,7 +6,7 @@ from nonebot.adapters.onebot.v11 import Bot
 from nonebot.adapters.onebot.v11.event import MessageEvent, GroupMessageEvent
 from .data_source import tarot_manager
 
-__tarot_version__ = "v0.4.0"
+__tarot_version__ = "v0.4.0.post1"
 __tarot_usages__ = f'''
 塔罗牌 {__tarot_version__}
 [占卜] 随机选取牌阵进行占卜
